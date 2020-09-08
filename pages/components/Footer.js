30a374dc8b7351959fee9b62c97927b424a1ab64
Heidi Dong <dong.heidi@gmail.com>
2020-09-07 18:48:03 -0700

    Add social media icons

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -9,7 +9,8 @@ export default function Footer() {
       </div>
       <footer className={styles.footer}>
         <div className="container gridPadding">
-    			Copyright © 2020 Innovative Designers of Tomorrow. All rights reserved.
+          <SocialMediaIcons />
+    			<p>Copyright © 2020 Innovative Designers of Tomorrow. All rights reserved.</p>
     		</div>
         <div className={styles.footerGraphicWrapper}>
           <img src="/img/footer-graphic.svg" alt="" className={styles.footerGraphic} />
@@ -18,3 +19,35 @@ export default function Footer() {
     </div>
   );
 };
+
+function SocialMediaIcons() {
+  return (
+    <div className={styles.socialMediaIcons}>
+      <a href="https://www.instagram.com/idot.club/" target="_blank">
+        <span className={styles.socialMediaIcon}>
+          <img src="/img/social-instagram.svg" alt="Instagram icon" title="Instagram" />
+        </span>
+      </a>
+      <a href="http://facebook.com/idotclubFB" target="_blank">
+        <span className={styles.socialMediaIcon}>
+          <img src="/img/social-facebook.svg" alt="Facebook icon" title="Facebook" />
+        </span>
+      </a>
+      <a href="http://linkedin.com/company/idotclub" target="_blank">
+        <span className={styles.socialMediaIcon}>
+          <img src="/img/social-linkedin.svg" alt="LinkedIn icon" title="LinkedIn" />
+        </span>
+      </a>
+      <a href="https://twitter.com/IDOTclub" target="_blank">
+        <span className={styles.socialMediaIcon}>
+          <img src="/img/social-twitter.svg" alt="Twitter icon" title="Twitter" />
+        </span>
+      </a>
+      <a href="http://medium.com/@IDOTclub" target="_blank">
+        <span className={styles.socialMediaIcon}>
+          <img src="/img/social-medium.svg" alt="Medium icon" title="Medium" />
+        </span>
+      </a>
+    </div>
+  );
+}
